7b0e71adec1cc31af2e253144d7a3deeae6b1f91
yuna17c <yuna0617c@gmail.com>
2024-08-14 15:31:41 -0400

    Change to post request with gpt4o

--- ai-writing/pages/api/generate-pos.js
+++ ai-writing/pages/api/generate-pos.js
@@ -1,15 +1,9 @@
 require('dotenv').config({path: '../../.env'});
-const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
-const apiKey = process.env.AZURE_OPENAI_KEY;
-const apiVersion = '2023-05-15';
-const { AzureOpenAI } = require("openai");
+import OpenAI from "openai";
 
 export default async function handler(req, res) {
-  const client = new AzureOpenAI(
-    { endpoint, apiKey, apiVersion }
-  );
-  const prompt = req.query.prompt
-  console.log("received:", prompt)
+  const client = new OpenAI();
+  const { prompt } = req.body;
   const system_instr = `You are a helpful writing agent who helps users complete their emails. Do not use placeholders in generation, in case of missing information, generate a seudo information to fill in the gap. You write in a way that is characteriszed as positve politeness. Positive politeness higlights friendliness and intimacy, and it has the following strategies: Strategy 1: Notice hearer's admirable qualities or possessions, show interest, exaggerate. Words that are commonly associated with positive politeness: 'marvelous, fantastic, extraordinary, wonderful, delightful, ravishing, divine, incredible, appalling, ghastly, devastating, outrageous, despicable, revolting, ridiculous, incredible, absolutely, completely'. Example: 'Hey love your new Palm-pilot, can I borrow it sometime?' 'How absolutely marvelous/extraordinary/devastating/incredible!' Strategy 2: Employ phonological slurring to convey in-group membership. Example: 'Heya, gimme a hand with this willya?' Strategy 3: Use colloquialisms or slang to convey in-group membership. Example: 'Most are damn hard, but this one should be a piece-of-cake.' Strategy 4: Use ellipsis (omission) to communicate tacit understandings. Example: '(Do you) mind if I join you?' Strategy 5: Use first name or in-group name (mate, buddy, pal, honey, dear, brother, sister, etc.) to insinuate familiarity. Example: 'Hey Bud, have you gotta minute?' 'Hey Johnny, how's it going?' Strategy 6: Claim common view: assert knowledge of hearer's wants or that hearer has knowledge of speaker's wants. Example: 'You know how the janitors don't like it when...' Strategy 7: Seek agreement; raise or presuppose common ground/ common values (use so, then, to refer to a fake prior agreement, pressuring the addressee to accept the request). Example : 'I'll be seeing you then.' 'So when are you coming to see us?'
     Strategy 8: Hedging opinions: use hedging to be vague about disagreeing opinions. Examples: 'It's really beautiful, in a way.' 'I don't know, like I think people have a right to their own opinions.'
     Strategy 9: Engage in small talk/ joke. Example: 'How bout that game last night? Did the Ravens whip the pants off the Giants or what!' ‘ Strategy 10: Give or ask for reasons: assert reflexivity by making activity seem reasonable to the hearer. Example: 'I'm really late for an important appointment, so ...' 'Why not lend me your cottage for the weekend?' Strategy 11: Use inclusive forms ('we' or 'lets') to include both speaker and hearer in the activity, or use 'you know'. Example: 'We're not feeling well, are we?' 'I really had a hard time learning to drive, you know.' 'Let's get on with dinner'
@@ -23,21 +17,13 @@ export default async function handler(req, res) {
         {"role": "system", "content": instruction},
         {"role": "user", "content": prompt},
       ],
-      model: 'gpt4',
-      max_tokens: 32,
+      model: 'gpt-4o',
+      max_tokens: 30,
       temperature: 1,
       n: 3,
       top_p: 0.55
     });
-    let val = response.choices[0].message.content
-    // Cut to a single sentence
-    console.log(val)
-    const indices = [val.indexOf('.'), val.indexOf('!'), val.indexOf('?')];
-    const validIndices = indices.filter(index => index !== -1);
-    let periodIdx = Math.min(...validIndices);
-    if (periodIdx!==-1) {
-      val = val.substring(0,periodIdx+1)
-    }
+    const val = response.choices[0].message.content
     res.status(200).json({ response: val });
   } catch (error) {
     console.error(error);
